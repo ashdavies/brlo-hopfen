ed96742bb763d577378f2c194b49ef3d4518d1e8
Ash Davies <ash.davies@gmail.com>
2017-09-30 15:36:05 +0200

    Merging the merge

--- feature/src/main/kotlin/de/brlo/hopfen/feature/home/HomeActivity.kt
+++ feature/src/main/kotlin/de/brlo/hopfen/feature/home/HomeActivity.kt
@@ -4,6 +4,8 @@ import android.arch.lifecycle.ViewModelProvider
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.support.annotation.FontRes
+import android.support.v4.content.res.ResourcesCompat
 import com.google.firebase.auth.FirebaseAuth
 import dagger.android.support.DaggerAppCompatActivity
 import de.brlo.hopfen.feature.BuildConfig
@@ -11,9 +13,12 @@ import de.brlo.hopfen.feature.R
 import de.brlo.hopfen.feature.activity.ActivityCompanion
 import de.brlo.hopfen.feature.databinding.ActivityHomeBinding
 import de.brlo.hopfen.feature.extensions.getDataBinding
+import de.brlo.hopfen.feature.extensions.getFont
 import de.brlo.hopfen.feature.extensions.getViewModel
 import de.brlo.hopfen.feature.home.HomeActivity.Companion.IntentOptions
 import de.brlo.hopfen.feature.login.LoginActivity
+import kotlinx.android.synthetic.main.activity_home.collapsing
+import kotlinx.android.synthetic.main.activity_home.toolbar
 import javax.inject.Inject
 
 class HomeActivity : DaggerAppCompatActivity() {
@@ -24,11 +29,19 @@ class HomeActivity : DaggerAppCompatActivity() {
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
+    setSupportActionBar(toolbar)
+
+    if (!BuildConfig.DEBUG && FirebaseAuth.getInstance().isNotLoggedIn()) onBackPressed()
 
     binding = getDataBinding(R.layout.activity_home)
     binding.model = getViewModel(factory)
 
-    if (!BuildConfig.DEBUG && FirebaseAuth.getInstance().isNotLoggedIn()) onBackPressed()
+    setCollapsingToolbarFont()
+  }
+
+  private fun setCollapsingToolbarFont() {
+    collapsing.setCollapsedTitleTypeface(getFont(R.font.product_sans_regular))
+    collapsing.setExpandedTitleTypeface(getFont(R.font.product_sans_regular))
   }
 
   override fun onBackPressed() {
