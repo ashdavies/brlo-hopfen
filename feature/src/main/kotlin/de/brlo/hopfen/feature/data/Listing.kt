77cb20bee6f5a3bceb682a7fea0985b20f3f9a2f
Mitchell Verzano Berg <mitchell.v.berg@gmail.com>
2017-09-30 16:06:09 +0200

    modifying listing to only use hops

--- feature/src/main/kotlin/de/brlo/hopfen/feature/data/Listing.kt
+++ feature/src/main/kotlin/de/brlo/hopfen/feature/data/Listing.kt
@@ -2,17 +2,8 @@ package de.brlo.hopfen.feature.data
 
 data class Listing(
         val uuid: String = "",
-        val name: String,
-        val description: String,
-        val listingType: Type,
+        val hop: Hop,
+        val location: Location,
         val quantity: Double,
         val quantityUnits: String,
-        val price: Double) {
-
-    enum class Type {
-        HOPS,
-        BARLEY,
-        SPENT_GRAIN
-    }
-
-}
+        val price: Double)
--- feature/src/main/kotlin/de/brlo/hopfen/feature/data/Location.kt
+++ feature/src/main/kotlin/de/brlo/hopfen/feature/data/Location.kt
@@ -2,5 +2,5 @@ package de.brlo.hopfen.feature.data
 
 data class Location(
         val uuid: String = "",
-        val account: String,
+        val account: Account,
         val address: String)
