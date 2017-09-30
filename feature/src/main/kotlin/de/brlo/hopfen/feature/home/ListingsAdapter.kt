b3af94c3fa0dd466f6a4d966b378e1319702b8da
Ash Davies <ash.davies@gmail.com>
2017-09-30 21:40:56 +0200

    fix

--- feature/src/main/kotlin/de/brlo/hopfen/feature/home/ListingsAdapter.kt
+++ feature/src/main/kotlin/de/brlo/hopfen/feature/home/ListingsAdapter.kt
@@ -1,17 +1,19 @@
 package de.brlo.hopfen.feature.home
 
+import android.databinding.DataBindingUtil
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import de.brlo.hopfen.feature.R
 import de.brlo.hopfen.feature.android.AbstractAdapter
 import de.brlo.hopfen.feature.data.Listing
+import de.brlo.hopfen.feature.databinding.ItemListingBinding
 import javax.inject.Inject
 
 internal class ListingsAdapter @Inject constructor(inflater: LayoutInflater) : AbstractAdapter<ListingsViewHolder, Listing>(inflater) {
 
-  override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = ListingsViewHolder(inflate(parent))
+  override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = ListingsViewHolder(parent.binding())
 
-  private fun inflate(parent: ViewGroup) = inflater.inflate(R.layout.item_listing, parent, false)
+  private fun ViewGroup.binding() = DataBindingUtil.inflate<ItemListingBinding>(inflater, R.layout.item_listing, this, false)
 
   override fun notifyDataSetChanged(old: MutableList<Listing>, new: MutableList<Listing>) = notifyDataSetChanged()
 }
--- feature/src/main/kotlin/de/brlo/hopfen/feature/home/ListingsViewHolder.kt
+++ feature/src/main/kotlin/de/brlo/hopfen/feature/home/ListingsViewHolder.kt
@@ -2,15 +2,14 @@ package de.brlo.hopfen.feature.home
 
 import android.view.View
 import de.brlo.hopfen.feature.android.AbstractAdapter
-import de.brlo.hopfen.feature.android.ViewModel
 import de.brlo.hopfen.feature.data.Listing
 import de.brlo.hopfen.feature.databinding.ItemListingBinding
 
-internal class ListingsViewHolder(view: View) : AbstractAdapter.ViewHolder<Listing>(view) {
+internal class ListingsViewHolder private constructor(private val binding: ItemListingBinding, view: View) : AbstractAdapter.ViewHolder<Listing>(view) {
 
-  private lateinit var binding: ItemListingBinding
+  constructor(binding: ItemListingBinding) : this(binding, binding.root)
 
-  override fun bind(t: Listing) = Unit
-
-  class ListingViewModel : ViewModel<Listing>()
+  override fun bind(t: Listing) {
+    binding.listing = t
+  }
 }
