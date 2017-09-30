670f61622b7132a91f673310c1b4be508fec627a
Ash Davies <ash.davies@gmail.com>
2017-09-30 21:32:30 +0200

    Yes, I was being sarcastic.

--- feature/src/main/kotlin/de/brlo/hopfen/feature/home/ListingsAdapter.kt
+++ feature/src/main/kotlin/de/brlo/hopfen/feature/home/ListingsAdapter.kt
@@ -1,6 +1,5 @@
 package de.brlo.hopfen.feature.home
 
-import android.support.v7.util.DiffUtil
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import de.brlo.hopfen.feature.R
@@ -14,8 +13,5 @@ internal class ListingsAdapter @Inject constructor(inflater: LayoutInflater) : A
 
   private fun inflate(parent: ViewGroup) = inflater.inflate(R.layout.item_listing, parent, false)
 
-  override fun notifyDataSetChanged(old: MutableList<Listing>, new: MutableList<Listing>) {
-    notifyDataSetChanged()
-//    DiffUtil.calculateDiff(ListingsDiffUtilCallback(old, new)).dispatchUpdatesTo(this)
-  }
+  override fun notifyDataSetChanged(old: MutableList<Listing>, new: MutableList<Listing>) = notifyDataSetChanged()
 }
--- feature/src/main/kotlin/de/brlo/hopfen/feature/home/ListingsViewHolder.kt
+++ feature/src/main/kotlin/de/brlo/hopfen/feature/home/ListingsViewHolder.kt
@@ -2,9 +2,15 @@ package de.brlo.hopfen.feature.home
 
 import android.view.View
 import de.brlo.hopfen.feature.android.AbstractAdapter
+import de.brlo.hopfen.feature.android.ViewModel
 import de.brlo.hopfen.feature.data.Listing
+import de.brlo.hopfen.feature.databinding.ItemListingBinding
 
 internal class ListingsViewHolder(view: View) : AbstractAdapter.ViewHolder<Listing>(view) {
 
+  private lateinit var binding: ItemListingBinding
+
   override fun bind(t: Listing) = Unit
+
+  class ListingViewModel : ViewModel<Listing>()
 }
